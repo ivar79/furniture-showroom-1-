723b70a32ef00f20cd0713763eb1f0d150b001b3
ivar79 <iska1398@gmail.com>
2026-06-20 04:46:52 -0700

    Add new features: Admin dashboard, image upload logic and page updates

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
-// Load env variables
+// خواندن متغیرهای محیطی از فایل .env
 dotenv.config();
 
 const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
-  throw new Error("DATABASE_URL must be set in environment variables.");
+  throw new Error("DATABASE_URL variable is not set in your .env file.");
 }
 
 export default defineConfig({
